930073848b97290e717a1081fc8eaa7e25754d67
Boulimaalx <yasminejoudi2@gmail.com>
2023-04-25 11:46:33 -0700

    Task 0 commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,10 @@
 #include "main.h"
 int main(void)
+/**
+ * main entry point
+ * returns 0
+ */
+
 {
 _putchar('_');
 _putchar('p');
@@ -10,5 +15,5 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return(0);
+return (0);
 }
