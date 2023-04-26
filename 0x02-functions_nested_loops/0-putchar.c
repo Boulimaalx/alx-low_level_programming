13271e2d4ce5e3ffe3dea32f75820e53384521b2
Boulimaalx <yasminejoudi2@gmail.com>
2023-04-26 12:05:07 -0700

    0 task commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
-int main(void)
 /**
  * main entry point
  * returns 0
  */
-
+int main (void)
 {
 _putchar('_');
 _putchar('p');
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,4 +1,10 @@
+#include "main.h"
 #include <unistd.h>
+/**
+ * _putchar writes the character c to stdout
+ * @c: The character to print
+ * return 1 on success, else -1
+ */
 int _putchar(char c)
 {
 	return (write(1,&c,1));
