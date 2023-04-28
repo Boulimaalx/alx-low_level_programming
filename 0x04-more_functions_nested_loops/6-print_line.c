01fb2d98f24eeac017f1c884515ba70bbebdca5f
Boulimaalx <yasminejoudi2@gmail.com>
2023-04-28 07:47:48 -0700

    print line commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,7 +9,7 @@ void print_line(int n)
 {
 if (n <= 0)
 {
-_putchar('n');
+_putchar('\n');
 }
 else
 {
@@ -19,6 +19,6 @@ for (i = 1; i <= n; i++)
 {
 _putchar('_');
 }
-_putchar('n');
+_putchar('\n');
 }
 }
