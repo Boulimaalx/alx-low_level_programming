5c5a094bcacdfbf828050c4049dc84f8d9bed983
Boulimaalx <yasminejoudi2@gmail.com>
2023-05-05 15:24:59 -0700

    strcat commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strcat - Entry point
  * Description: function _strcat concatenates two strings
@@ -6,13 +7,17 @@
  * @src: input value
  * Return: void
  */
-
 char *_strcat(char *dest, char *src)
 {
 int i;
 int j;
 
 i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+j = 0;
 while (src[j] != '\0')
 {
 dest[i] = src[j];
